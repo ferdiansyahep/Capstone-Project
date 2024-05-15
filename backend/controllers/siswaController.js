8a59145425e6f454f4c8fca0ecbb0d1fa6015279
Wibiemahardhika22 <wibie2100016081@webmail.uad.ac.id>
2024-05-15 17:39:52 +0700

    feat: menambahkan fungsi yang berhubungan dengan kelas

--- backend/controllers/siswaController.js
+++ backend/controllers/siswaController.js
@@ -1,29 +1,43 @@
 import Siswa from "../models/siswaModel.js";
 import argon2 from "argon2";
 import generateToken from "../config/generateToken.js";
+import Kelas from "../models/kelasModel.js";
 
 const createSiswa = async (req, res) => {
-    const { nis, nama, password, gender, kelas, tempat_lahir, tanggal_lahir, nama_ortu, alamat } = req.body;
-
     try {
         // Periksa apakah NIS sudah terdaftar sebelumnya
-        const existingSiswa = await Siswa.findOne({ where: { nis: nis } });
+        const existingSiswa = await Siswa.findOne({ where: { nis: req.body.nis } });
         if (existingSiswa) {
             return res.status(400).json({ message: 'NIS sudah terdaftar. Silahkan gunakan NIS lain.' });
         }
 
-        const hashedPassword = await argon2.hash(password);
+        // Temukan ID kelas berdasarkan nama kelas
+        const kelasData = await Kelas.findOne({ where: { nama_kelas: req.body.kelas } });
+        if (!kelasData) {
+            return res.status(404).json({ message: 'Kelas tidak ditemukan' });
+        }
+
+        const hashedPassword = await argon2.hash(req.body.password);
         const newSiswa = await Siswa.create({
-            nis: nis,
-            nama: nama,
-            gender: gender,
+            nis: req.body.nis,
+            nama: req.body.nama,
+            gender: req.body.gender,
             password: hashedPassword,
-            kelas: kelas,
-            tempat_lahir: tempat_lahir,
-            tanggal_lahir: tanggal_lahir,
-            nama_ortu: nama_ortu,
-            alamat: alamat,
+            kelas: req.body.kelas,
+            tempat_lahir: req.body.tempat_lahir,
+            tanggal_lahir: req.body.tanggal_lahir,
+            nama_ortu: req.body.nama_ortu,
+            alamat: req.body.alamat,
         });
+
+        // Update jumlah siswa di kelas yang sesuai
+        if (req.body.gender === 'L') {
+            kelasData.jumlah_laki_laki += 1;
+        } else if (req.body.gender === 'P') {
+            kelasData.jumlah_perempuan += 1;
+        }
+        await kelasData.save();
+
         const token = generateToken(newSiswa.id);
         res.status(201).json({ newSiswa, token });
     } catch (error) {
@@ -70,25 +84,99 @@ const getSiswaById = async (req, res) => {
 };
 
 const updateSiswa = async (req, res) => {
-    const { nis, nama, password, kelas, tempat_lahir, tanggal_lahir, nama_ortu, alamat } = req.body;
+    const {
+        nama,
+        gender,
+        kelas: newKelas,
+        tempat_lahir,
+        tanggal_lahir,
+        nama_ortu,
+        alamat
+    } = req.body;
 
     try {
         const siswa = await Siswa.findByPk(req.params.id);
-        if (siswa) {
-            siswa.nis = nis || siswa.nis;
-            siswa.nama = nama || siswa.nama;
-            siswa.password = password ? await argon2.hash(password) : siswa.password;
-            siswa.kelas = kelas || siswa.kelas;
-            siswa.tempat_lahir = tempat_lahir || siswa.tempat_lahir;
-            siswa.tanggal_lahir = tanggal_lahir || siswa.tanggal_lahir;
-            siswa.nama_ortu = nama_ortu || siswa.nama_ortu;
-            siswa.alamat = alamat || siswa.alamat;
-
-            await siswa.save();
-            res.json({ message: "Student updated", siswa });
-        } else {
-            res.status(404).json({ message: 'Student not found' });
+        if (!siswa) {
+            return res.status(404).json({ message: 'Siswa tidak ditemukan' });
         }
+
+        // Memperbarui kelas
+        if (newKelas && newKelas !== siswa.kelas) {
+            // Memperbarui kelas siswa sebelumnya
+            const previousClassData = await Kelas.findOne({ where: { nama_kelas: siswa.kelas } });
+            if (previousClassData) {
+                if (siswa.gender === 'L') {
+                    previousClassData.jumlah_laki_laki -= 1;
+                } else if (siswa.gender === 'P') {
+                    previousClassData.jumlah_perempuan -= 1;
+                }
+                previousClassData.total_siswa -= 1;
+                await previousClassData.save();
+            }
+
+            // Memperbarui kelas baru siswa
+            const newClassData = await Kelas.findOne({ where: { nama_kelas: newKelas } });
+            if (newClassData) {
+                if (siswa.gender === 'L') {
+                    newClassData.jumlah_laki_laki += 1;
+                } else if (siswa.gender === 'P') {
+                    newClassData.jumlah_perempuan += 1;
+                }
+                newClassData.total_siswa += 1;
+                await newClassData.save();
+            }
+
+            siswa.kelas = newKelas;
+        }
+
+        // Memperbarui jenis kelamin
+        if (gender) {
+            // Periksa perubahan jenis kelamin dan perbarui kelas yang sesuai
+            if (gender !== siswa.gender) {
+                const previousClass = siswa.kelas;
+                const previousGender = siswa.gender;
+
+                if (previousGender === 'L') {
+                    const previousClassData = await Kelas.findOne({ where: { nama_kelas: previousClass } });
+                    if (previousClassData) {
+                        previousClassData.jumlah_laki_laki -= 1;
+                        await previousClassData.save();
+                    }
+                } else if (previousGender === 'P') {
+                    const previousClassData = await Kelas.findOne({ where: { nama_kelas: previousClass } });
+                    if (previousClassData) {
+                        previousClassData.jumlah_perempuan -= 1;
+                        await previousClassData.save();
+                    }
+                }
+
+                if (gender === 'L') {
+                    const newClassData = await Kelas.findOne({ where: { nama_kelas: newKelas } });
+                    if (newClassData) {
+                        newClassData.jumlah_laki_laki += 1;
+                        await newClassData.save();
+                    }
+                } else if (gender === 'P') {
+                    const newClassData = await Kelas.findOne({ where: { nama_kelas: newKelas } });
+                    if (newClassData) {
+                        newClassData.jumlah_perempuan += 1;
+                        await newClassData.save();
+                    }
+                }
+            }
+
+            siswa.gender = gender;
+        }
+
+        // Perbarui kolom lainnya
+        siswa.nama = nama || siswa.nama;
+        siswa.tempat_lahir = tempat_lahir || siswa.tempat_lahir;
+        siswa.tanggal_lahir = tanggal_lahir || siswa.tanggal_lahir;
+        siswa.nama_ortu = nama_ortu || siswa.nama_ortu;
+        siswa.alamat = alamat || siswa.alamat;
+        await siswa.save();
+
+        res.json({ message: "Siswa diperbarui", siswa });
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
@@ -98,6 +186,22 @@ const deleteSiswa = async (req, res) => {
     try {
         const siswa = await Siswa.findByPk(req.params.id);
         if (siswa) {
+            const kelasSiswa = await Kelas.findOne({ where: { nama_kelas: siswa.kelas } });
+
+            if (kelasSiswa) {
+                if (siswa.gender === 'L' && kelasSiswa.jumlah_laki_laki > 0) {
+                    kelasSiswa.jumlah_laki_laki -= 1;
+
+                } else if (siswa.gender === 'P' && kelasSiswa.jumlah_perempuan > 0) {
+                    kelasSiswa.jumlah_perempuan -= 1;
+
+                }
+                kelasSiswa.jumlah_laki_laki = Math.max(0, kelasSiswa.jumlah_laki_laki);
+                kelasSiswa.jumlah_perempuan = Math.max(0, kelasSiswa.jumlah_perempuan);
+
+                await kelasSiswa.save();
+            }
+
             await siswa.destroy();
             res.status(200).json({ message: 'Student removed' });
         } else {
